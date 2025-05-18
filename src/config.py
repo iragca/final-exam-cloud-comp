51e8c13d8b83c5844a206ace95c0dcc81076808d
iragca <irag.chrisandrei@mailbox.org>
2025-05-18 12:13:56 +0800

    Refactor environment variable loading and validation for clarity

--- src/config.py
+++ src/config.py
@@ -13,21 +13,21 @@ INTERIM_DATA_DIR = DATA_DIR / "interim"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 
 if not (PROJECT_ROOT / ".env").exists():
-    raise FileNotFoundError(
-        f"Please create a .env file in the root directory ({PROJECT_ROOT})"
-    )
-else:
-    load_dotenv(PROJECT_ROOT / ".env")
-    STAGING_DATABASE_URL = os.getenv("STAGING_DATABASE_URL")
-    WAREHOUSE_DATABASE_URL = os.getenv("WAREHOUSE_DATABASE_URL")
+    raise FileNotFoundError(f"Please create a .env file in the root directory ({PROJECT_ROOT})")
 
-    def check_env_variable(var, var_name: str):
-        """Check if an environment variable is set."""
-        if var is None:
-            raise EnvironmentError(f"Environment variable {var_name} is not set.")
+load_dotenv(PROJECT_ROOT / ".env")
+STAGING_DATABASE_URL = os.getenv("STAGING_DATABASE_URL")
+WAREHOUSE_DATABASE_URL = os.getenv("WAREHOUSE_DATABASE_URL")
 
-    check_env_variable(STAGING_DATABASE_URL, "STAGING_DATABASE_URL")
-    check_env_variable(WAREHOUSE_DATABASE_URL, "WAREHOUSE_DATABASE_URL")
+
+def check_env_variable(var, var_name: str):
+    """Check if an environment variable is set."""
+    if var is None:
+        raise EnvironmentError(f"Environment variable {var_name} is not set.")
+
+
+check_env_variable(STAGING_DATABASE_URL, "STAGING_DATABASE_URL")
+check_env_variable(WAREHOUSE_DATABASE_URL, "WAREHOUSE_DATABASE_URL")
 
 
 logger.info(f"PROJECT_ROOT: {PROJECT_ROOT}")
