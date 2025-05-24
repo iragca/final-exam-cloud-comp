8eabb06b790009eae00c890c9b00da1c46262faa
iragca <irag.chrisandrei@mailbox.org>
2025-05-24 20:27:11 +0800

    fix: replace dashes with underscores in product category extraction

--- src/utils/grab_product_category.py
+++ src/utils/grab_product_category.py
@@ -3,10 +3,10 @@ def grab_product_category(prd_key: str) -> str:
     Get the product category for a given product ID.
     Example:
         >> prd_key: CO-RF-FR-R92B-58
-        >> Product ID: CO-RF
+        >> Product ID: CO_RF
     """
     try:
-        return prd_key[:5]
+        return prd_key[:5].replace("-", "_")
     except Exception as e:
         print(f"Error retrieving category for product {prd_key}: {e}")
         return None
